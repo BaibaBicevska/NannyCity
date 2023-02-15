d26b6018e8bde4a903c2a8a8ad9bdadb308bcc88
Zanna Klimova <petraa@inbox.lv>
2023-02-15 19:23:06 +0200

    added changes

--- src/main/java/com/nannycity/controllers/NannyUserController.java
+++ src/main/java/com/nannycity/controllers/NannyUserController.java
@@ -10,8 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static javax.swing.text.html.HTML.Attribute.N;
 
 @Controller
 public class NannyUserController {
@@ -36,7 +42,7 @@ public class NannyUserController {
             Cookie cookie = new Cookie("userId", loggedInUser.getId().toString());
             response.addCookie(cookie);
             response.addCookie(new Cookie("userIsLoggedIn", "true"));
-            return "redirect:nannyProfile"/* + loggedInUser.getId()*/;
+            return "redirect:nannyProfile/" + loggedInUser.getId();
         } catch (Exception e) {
             return "redirect:login?message=login_failed&error=" + e.getMessage();
         }
@@ -88,13 +94,27 @@ public class NannyUserController {
         return "nanniesAdmin";
     }
 
-  @GetMapping("/nannyProfile")
-    public String showNannyProfile () {
+    @GetMapping("/nannyProfile/{id}")
+    public String nanniesProfileDetails(@PathVariable(value = "id") long id, Model model) {
+        Optional<NannyUser> nannyUser = nannyRepository.findById(id);
+        ArrayList<NannyUser> result = new ArrayList<>();
+        nannyUser.ifPresent(result::add);
+        model.addAttribute("nannyUser", result);
         return "nannyProfile";
     }
 
-    private final UserService userService;
+    /*@GetMapping("/nannyProfile/{id}")
+    public String addNannie(@PathVariable(value = "id") long id, Model model) {
+        Iterable<NannyUser> nannies = nannyRepository.findAll();
+        model.addAttribute("nannies", nannies);
+        return "nannyProfile";
+    }*/
 
+    /*@PostMapping("/nannyProfile/{id}")
+    public String addNannieeProfile(@PathVariable(value = "id") long id, Model model) {
+        return "nannyProfile";    }*/
+
+    private final UserService userService;
 
 }
 
